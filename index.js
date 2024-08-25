be61685e3b0a036ea85b2b2b2c5db82c16012bbf
ItsFranklinMyDudes <franklindudes21@gmail.com>
2024-08-25 12:45:32 +1000

    added brows

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 class Maker {
     addBase(number) {
+        if(!number) {
+            throw new Error('must be a value');
+        }
         if (typeof number !== 'number') {
             throw new Error('must be a number');
         }
         if(number > 61 || number < 0) {
+            throw new Error('number must be between 0 and 61');
+        }
+        return number;
+    }
+    addEyes(number) {
+        if (typeof number !== 'number') {
+            throw new Error('must be a number');
+        }
+        if(number > 63 || number < 0) {
             throw new Error('number must be between 0 and 61');
         }
         return number;
